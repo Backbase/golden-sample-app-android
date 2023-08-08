8ab3a9a4103f4e2b5697f724a5a165c8e91cf755
Ka Wing <kawing@backbase.com>
2023-08-08 15:29:48 +0200

    Move host and hello fragment to kotlin folder

--- app/src/main/kotlin/com/backbase/golden_sample_app/host/AppNavHostFragment.kt
+++ app/src/main/kotlin/com/backbase/golden_sample_app/host/AppNavHostFragment.kt
@@ -1,4 +1,4 @@
-package com.backbase.golden_sample_app
+package com.backbase.golden_sample_app.host
 
 import android.os.Bundle
 import android.view.LayoutInflater
--- app/src/main/kotlin/com/backbase/golden_sample_app/presentation/HelloFragment.kt
+++ app/src/main/kotlin/com/backbase/golden_sample_app/presentation/HelloFragment.kt
@@ -1,10 +1,11 @@
-package com.backbase.golden_sample_app
+package com.backbase.golden_sample_app.presentation
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.backbase.golden_sample_app.R
 
 /**
  * A simple [Fragment] subclass.
