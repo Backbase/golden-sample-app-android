e92d4dfb87e2f70b2ad7bb87dcbcca8666e7d15d
Ka Wing <kawing@backbase.com>
2023-08-15 15:50:27 +0200

    Deleted workaround retail journey common package and classes
    Moved UserRepositoryImpl outside common package

--- app/src/main/kotlin/com/backbase/golden_sample_app/user/UserRepositoryImpl.kt
+++ app/src/main/kotlin/com/backbase/golden_sample_app/user/UserRepositoryImpl.kt
@@ -1,4 +1,4 @@
-package com.backbase.golden_sample_app.common.user
+package com.backbase.golden_sample_app.user
 
 import androidx.annotation.VisibleForTesting
 import com.backbase.android.business.journey.common.user.User
