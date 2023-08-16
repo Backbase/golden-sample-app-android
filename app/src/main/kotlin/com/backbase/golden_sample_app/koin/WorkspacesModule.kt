2b0b48d116dccc9169b4489bb4f4e8390ed7fcb6
Ka Wing <kawing@backbase.com>
2023-08-16 15:55:59 +0200

    Deleted bitrise and SwitcherRouter. Nav host xml use FrameLayout

--- app/src/main/kotlin/com/backbase/golden_sample_app/koin/WorkspacesModule.kt
+++ app/src/main/kotlin/com/backbase/golden_sample_app/koin/WorkspacesModule.kt
@@ -2,14 +2,11 @@ package com.backbase.golden_sample_app.koin
 
 import com.backbase.android.business.journey.workspaces.accesscontrol_client_2.WorkspacesUseCaseImpl
 import com.backbase.android.business.journey.workspaces.navigation.WorkspaceSelectorRouting
-import com.backbase.android.business.journey.workspaces.navigation.WorkspaceSwitcherRouting
 import com.backbase.android.business.journey.workspaces.usecase.WorkspacesUseCase
 import com.backbase.golden_sample_app.router.WorkspaceSelectorRoutingImpl
-import com.backbase.golden_sample_app.router.WorkspaceSwitcherRouterImpl
 import org.koin.dsl.module
 
 internal val workspacesModule = module {
     factory<WorkspacesUseCase> { WorkspacesUseCaseImpl(get(), get()) }
     factory<WorkspaceSelectorRouting> { WorkspaceSelectorRoutingImpl(get(), get()) }
-    single<WorkspaceSwitcherRouting> { WorkspaceSwitcherRouterImpl() }
 }
