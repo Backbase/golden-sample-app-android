8df2992d00492859841039c3efcdbc46bfb3cdeb
Ka Wing <kawing@backbase.com>
2023-10-04 11:12:31 +0200

    Fixed, the precompiled script of feature and base was swapped between journey and use case modules.

--- accounts-use-case/build.gradle.kts
+++ accounts-use-case/build.gradle.kts
@@ -1,6 +1,6 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    id(backbase.plugins.feature.android.module.get().pluginId)
+    id(backbase.plugins.base.android.library.module.get().pluginId)
     id(backbase.plugins.configured.detekt.get().pluginId)
 }
 
