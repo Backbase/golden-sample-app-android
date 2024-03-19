8854cf71d28b5b92540f519289a23da4307883b4
dmitry.berendeev <dmitrybe@backbase.com>
2024-03-19 13:27:44 +0100

    remove decor

--- retail-app/src/main/java/com/backbase/android/retail/MainActivity.kt
+++ retail-app/src/main/java/com/backbase/android/retail/MainActivity.kt
@@ -2,12 +2,14 @@ package com.backbase.android.retail
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.core.view.WindowCompat
 import androidx.fragment.app.FragmentActivity
 import com.backbase.android.design.theme.PreviewTheme
 
 class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             PreviewTheme {
                 MainNavHost()
