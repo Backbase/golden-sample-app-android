871ab3264fedfa9d7d1ff967185354fc5d07b695
charbelmkh <charbel.c.makhlouf@gmail.com>
2024-07-25 11:27:27 +0200

    Moved configurations from app level to all project level

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,17 +61,6 @@ android {
             excludes += "META-INF/LICENSE-notice.md"
         }
     }
-
-    configurations.all {
-        resolutionStrategy {
-            // Reverting to an older version of objenesis due to build failure during androidTest using mockK
-            // Issue link => https://github.com/mockk/mockk/issues/281
-            force("org.objenesis:objenesis:2.6")
-            // Excluding to avoid "More than one file was found with OS independent path 'META-INF/AL2.0' & 'META-INF/LGPL2.1'"
-            // Issue link => https://github.com/Kotlin/kotlinx.coroutines/issues/2023
-            exclude(group = "org.jetbrains.kotlinx", module = "kotlinx-coroutines-debug")
-        }
-    }
 }
 
 dependencies {
