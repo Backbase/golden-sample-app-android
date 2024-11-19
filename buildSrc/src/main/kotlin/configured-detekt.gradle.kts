87a8a6d50a5c127c8aeedb97170589a0c6a8851d
Ka Wing <kawing@backbase.com>
2024-11-19 14:38:31 +0100

    Fixed detekt not using rootDir as starting path.

--- buildSrc/src/main/kotlin/configured-detekt.gradle.kts
+++ buildSrc/src/main/kotlin/configured-detekt.gradle.kts
@@ -10,7 +10,7 @@ detekt {
     toolVersion = "1.23.1"
     buildUponDefaultConfig = true // preconfigure defaults
     allRules = false // activate all available (even unstable) rules.
-    config.setFrom("../config/golden-sample-app-detekt.yml") // point to your custom config defining rules to run, overwriting default behavior
+    config.setFrom("$rootDir/config/golden-sample-app-detekt.yml") // point to your custom config defining rules to run, overwriting default behavior
     parallel = true
     autoCorrect = true
     ignoredVariants = listOf("release")
