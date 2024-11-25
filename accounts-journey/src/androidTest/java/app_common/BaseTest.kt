351dbd571ccdf9c504efbd6b5f1a02d2f573f13e
Ka Wing <kawing@backbase.com>
2024-11-25 16:51:03 +0100

    Fixed UI tests

--- accounts-journey/src/androidTest/java/app_common/BaseTest.kt
+++ accounts-journey/src/androidTest/java/app_common/BaseTest.kt
@@ -4,13 +4,7 @@ import android.content.Context
 import android.content.res.Configuration
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.platform.app.InstrumentationRegistry
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.TestDispatcher
-import kotlinx.coroutines.test.UnconfinedTestDispatcher
-import kotlinx.coroutines.test.resetMain
-import kotlinx.coroutines.test.setMain
-import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.koin.core.context.stopKoin
@@ -18,12 +12,8 @@ import org.koin.test.KoinTest
 import java.util.Locale
 
 
-
-
 @ExperimentalCoroutinesApi
 open class BaseTest : KoinTest {
-
-    private val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
 
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
@@ -33,19 +23,12 @@ open class BaseTest : KoinTest {
         stopKoin()
         // change the local to run the tests in different local
         setLocale("en")
-        Dispatchers.setMain(testDispatcher)
     }
 
-    @After
-    open fun tearDown() {
-        Dispatchers.resetMain()
-    }
-
-     fun setLocale(language: String) {
+    fun setLocale(language: String) {
         val context: Context = InstrumentationRegistry.getInstrumentation().getTargetContext()
         val config: Configuration = context.getResources().getConfiguration()
         config.setLocale(Locale(language))
         context.createConfigurationContext(config)
     }
-
 }
