5ff1b9615e7372d90f8a9c209a59cffb58fac1dd
WingEraser <292435+WingEraser@users.noreply.github.com>
2025-02-10 10:49:15 +0100

    Fixed able to navigate backstack while logged out or switched user.

--- app/src/main/kotlin/com/backbase/golden_sample_app/menu/MoreMenuModule.kt
+++ app/src/main/kotlin/com/backbase/golden_sample_app/menu/MoreMenuModule.kt
@@ -23,20 +23,27 @@ import org.koin.dsl.module
  * Created by Backbase R&D B.V. on 23/07/2020.
  */
 internal fun Module.moreMenuModule(block: MoreMenuDependenciesScope.() -> Unit) {
+    val dependencies = MoreMenuDependenciesScope().apply(block)
+
     scope<MoreJourneyScope> {
+        scoped(definition = dependencies.moreMenuJourneyConfiguration)
         factory<MoreRouter> {
             MoreMenuRouterImpl(get<NavController>())
         }
 
-        scoped { demoMoreConfig(get()) }
+        scoped {
+            demoMoreConfig(
+                sessionManager = get(),
+                navController = get<NavController>()
+            )
+        }
     }
 }
-
 
 internal fun moreMenuModule() = module {
     moreMenuModule {
         moreMenuJourneyConfiguration = {
-            demoMoreConfig(get())
+            demoMoreConfig(sessionManager = get(), navController = get())
         }
     }
 }
@@ -49,13 +56,14 @@ internal class MoreMenuDependenciesScope {
 }
 
 fun demoMoreConfig(
-    sessionManager: SessionManager
+    sessionManager: SessionManager,
+    navController: NavController
 ) = MoreConfiguration {
     showIcons = true
     contentDescription = DeferredText.Resource(R.string.more_menu_title)
     sections = MenuSections {
         +demoSection()
-        +logOutSection(sessionManager)
+        +logOutSection(sessionManager, navController)
     }
 }
 
@@ -76,7 +84,8 @@ private fun demoSection(): MenuSection {
 }
 
 private fun logOutSection(
-    sessionManager: SessionManager
+    sessionManager: SessionManager,
+    navController: NavController
 ) = MenuSection {
     val switchIconColor =
         DeferredColor.Resource(com.backbase.android.design.R.color.bds_onDanger)
@@ -85,22 +94,27 @@ private fun logOutSection(
         icon = DeferredDrawable.Resource(com.backbase.android.design.R.drawable.backbase_ic_logout) {
             setTint(switchIconColor.resolve(it))
         },
-        iconBackgroundColor = DeferredColor.Resource(com.backbase.android.design.R.color.bds_primary)
-    ) {
-        sessionManager.logOut()
-        BackToAuth(AuthenticationJourney.LAUNCH_ACTION_END_SESSION)
-    }
+        iconBackgroundColor = DeferredColor.Resource(com.backbase.android.design.R.color.bds_primary),
+        actionBlock = {
+            sessionManager.logOut()
+            navController.popBackStack(R.id.workspaces_selector, true)
+            BackToAuth(AuthenticationJourney.LAUNCH_ACTION_END_SESSION)
+        }
+    )
     +MenuItem(
         title = DeferredText.Resource(R.string.more_menu_switch_user),
         icon = DeferredDrawable.Resource(com.backbase.android.design.R.drawable.backbase_ic_person) {
             setTint(switchIconColor.resolve(it))
         },
-        iconBackgroundColor = DeferredColor.Resource(com.backbase.android.design.R.color.bds_danger)
-    ) {
-        sessionManager.switchUser()
-        BackToAuth(AuthenticationJourney.LAUNCH_ACTION_LOG_OUT)
-    }
+        iconBackgroundColor = DeferredColor.Resource(com.backbase.android.design.R.color.bds_danger),
+        actionBlock = {
+            sessionManager.switchUser()
+            navController.popBackStack(R.id.workspaces_selector, true)
+            BackToAuth(AuthenticationJourney.LAUNCH_ACTION_LOG_OUT)
+        }
+    )
     title = DeferredText.Resource(R.string.more_menu_security_section_title)
 }
 
-internal class BackToAuth(arg: String) : NavigateTo(R.id.authenticationJourney, bundleOf(arg to true))
+internal class BackToAuth(arg: String) :
+    NavigateTo(R.id.authenticationJourney, bundleOf(arg to true))
--- app/src/main/kotlin/com/backbase/golden_sample_app/menu/MoreMenuRouterImpl.kt
+++ app/src/main/kotlin/com/backbase/golden_sample_app/menu/MoreMenuRouterImpl.kt
@@ -6,7 +6,6 @@ import com.backbase.android.retail.journey.more.MoreRouter
 
 class MoreMenuRouterImpl(
     private val navController: NavController
-
 ) : MoreRouter {
     override fun onExit(navigationActionId: Int, args: Bundle?) {
         navController.navigate(navigationActionId, args)
