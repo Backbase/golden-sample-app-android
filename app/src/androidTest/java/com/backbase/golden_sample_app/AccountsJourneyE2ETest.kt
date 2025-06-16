120be8b0f0f46cc85d9ac63fc8d1b8d6d30e3c2b
Wing Eraser <292435+WingEraser@users.noreply.github.com>
2025-06-16 10:53:25 +0200

    Feature/mf 2271 L4 tests (#44)
    * Update BOM, journeys, use cases and added Secure Storage
    
    * Update config.json to 2025.03 lts
    
    * Fixed unit tests
    
    * Updated CATALOG
    
    * Added desugering to accounts journey
    
    * Added more UI tests for Accounts list.
    
    Refactor generators and stubs
    
    * renamed fake-accounts-use-case to accounts-test-data
    
    * added accounts-demo module
    
    accounts journey is runnable
    
    * Added AccountsJourney End 2 End tests
    
    * Fixed navigation test library not being fetched
    
    * Added E2E tests for app
    
    * Updated README about tests

---
+++ app/src/androidTest/java/com/backbase/golden_sample_app/AccountsJourneyE2ETest.kt
@@ -0,0 +1,73 @@
+package com.backbase.golden_sample_app
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.ViewInteraction
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.backbase.accounts_journey.R
+import com.backbase.android.test_data.hasItems
+import com.backbase.android.test_data.shouldBeDisplayed
+import com.backbase.android.test_data.shouldMatchText
+import com.backbase.android.test_data.typeTextInInput
+import com.backbase.android.test_data.waitForRecyclerViewToHaveItems
+import com.backbase.android.test_data.waitForView
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class AccountsJourneyE2ETest : BaseE2ETest() {
+
+    private val accountHeader: ViewInteraction = onView(withId(R.id.header))
+    private val accountList: ViewInteraction = onView(withId(R.id.accountlist))
+    private val searchInput: ViewInteraction = onView(withId(R.id.searchTextInput))
+    private val noAccountImg: ViewInteraction = onView(withId(R.id.no_account_image))
+    private val accountsSearchResultTxt: ViewInteraction = onView(withId(R.id.accounts_result_text))
+
+    private val accountDetailHeader: ViewInteraction = onView(withId(R.id.header_account))
+
+    @Before
+    fun setUp() {
+        login()
+        setupPasscode()
+        accountList.waitForRecyclerViewToHaveItems()
+    }
+
+    @Test
+    fun accountListShouldBeDisplayed() {
+        accountHeader.shouldBeDisplayed()
+        accountList.shouldBeDisplayed()
+        accountList.check(hasItems())
+    }
+
+    @Test
+    fun successfulSearchAccountsAreDisplayed() {
+        searchInput.typeTextInInput("Sara")
+
+        accountList.check(matches(hasDescendant(withText("Sara's Current Account 1"))))
+    }
+
+    @Test
+    fun emptySearchResultIsDisplayed() {
+        searchInput.typeTextInInput("Search query without result")
+
+        noAccountImg.shouldBeDisplayed()
+        accountsSearchResultTxt.shouldMatchText(R.string.no_accounts)
+    }
+
+    @Test
+    fun accountDetailShouldBeDisplayed() {
+        accountList.perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(1, click())
+        )
+        accountDetailHeader.waitForView()
+
+        accountDetailHeader.shouldBeDisplayed()
+    }
+}
